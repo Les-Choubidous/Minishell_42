ab1fcae2d3f3fcf8f57e6046166f02467b42675c
uzanchi <ugozanchi5@gmail.com>
2025-01-20 16:23:40 +0100

    debut correction leaks expander

--- sources/2_parser/expander/expander_main.c
+++ sources/2_parser/expander/expander_main.c
@@ -3,14 +3,40 @@
 /*                                                        :::      ::::::::   */
 /*   expander_main.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: memotyle <memotyle@student.42.fr>          +#+  +:+       +#+        */
+/*   By: uzanchi <uzanchi@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/17 17:57:31 by memotyle          #+#    #+#             */
-/*   Updated: 2025/01/17 17:57:33 by memotyle         ###   ########.fr       */
+/*   Updated: 2025/01/20 16:19:48 by uzanchi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+
+// char	*expand_var_or_exit(t_data *data, char *var, int *i)
+// {
+// 	char	*before;
+// 	char	*in_var;
+
+// 	before = ft_strdup(data->expanded_str);
+// 	if (!before)
+// 		return (failed_mess(data, "malloc failed", 1), NULL);
+// 	if (ft_strcmp(var, "$?") == 0)
+// 	{
+// 		in_var = ft_itoa(data->exit_status);
+// 		(*i) += 2;
+// 	}
+// 	else if (is_exist_in_env(var, data))
+// 		in_var = give_me_inside_var(var, data);
+// 	else
+// 		in_var = ft_strdup("");
+// 	free(data->expanded_str);
+// 	data->expanded_str = ft_concatenate(before, in_var);
+// 	free(before);
+// 	free(in_var);
+// 	if (!data->expanded_str)
+// 		return (failed_mess(data, "malloc failed", 1), NULL);
+// 	return (data->expanded_str);
+// }
 
 char	*expand_var_or_exit(t_data *data, char *var, int *i)
 {
@@ -20,6 +46,7 @@ char	*expand_var_or_exit(t_data *data, char *var, int *i)
 	before = ft_strdup(data->expanded_str);
 	if (!before)
 		return (failed_mess(data, "malloc failed", 1), NULL);
+
 	if (ft_strcmp(var, "$?") == 0)
 	{
 		in_var = ft_itoa(data->exit_status);
@@ -29,12 +56,18 @@ char	*expand_var_or_exit(t_data *data, char *var, int *i)
 		in_var = give_me_inside_var(var, data);
 	else
 		in_var = ft_strdup("");
+
+	// Libération systématique, car var est toujours dynamique
+	free(var);
+
 	free(data->expanded_str);
 	data->expanded_str = ft_concatenate(before, in_var);
 	free(before);
 	free(in_var);
+
 	if (!data->expanded_str)
 		return (failed_mess(data, "malloc failed", 1), NULL);
+
 	return (data->expanded_str);
 }
 
@@ -54,12 +87,34 @@ char	*process_character(t_data *data, char *str, int *i)
 	return (data->expanded_str);
 }
 
+// char	*process_expansion(char *str, t_data *data, int *i)
+// {
+// 	char	*var;
+
+// 	if (str[*i] == '$' && str[*i + 1] == '?' && !is_in_single_quotes(str, *i))
+// 		return (expand_var_or_exit(data, "$?", i));
+// 	if (str[*i] == '$' && !is_in_single_quotes(str, *i))
+// 	{
+// 		var = extract_var(str + *i, i);
+// 		if (!var)
+// 			return (failed_mess(data, "Failed to extract variable", 1), NULL);
+// 		return (expand_var_or_exit(data, var, i));
+// 	}
+// 	return (process_character(data, str, i));
+// }
+
 char	*process_expansion(char *str, t_data *data, int *i)
 {
 	char	*var;
 
 	if (str[*i] == '$' && str[*i + 1] == '?' && !is_in_single_quotes(str, *i))
-		return (expand_var_or_exit(data, "$?", i));
+	{
+		// Allouer dynamiquement pour éviter les problèmes de free
+		var = ft_strdup("$?");
+		if (!var)
+			return (failed_mess(data, "malloc failed", 1), NULL);
+		return (expand_var_or_exit(data, var, i));
+	}
 	if (str[*i] == '$' && !is_in_single_quotes(str, *i))
 	{
 		var = extract_var(str + *i, i);
